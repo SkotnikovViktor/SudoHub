dbc5c6fdc785720d619851e36004b60a5f667141
WasiliiSizov <V8261261@gmail.com>
2026-03-18 21:10:15 +0300

    перенесины все функции в библеотеку Alg.C и Alg.H

--- Clib/razr.c
+++ Clib/razr.c
@@ -1,48 +1,8 @@
+#include"Alg.H"
+#include <string.h> 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-int* countSpacesBeforeEachDot(const char *text, int *dotCount) {
-    int len = strlen(text);
-    int *spaceCounts = NULL;
-    int count = 0;
-    int spaceCounter = 0;
-    int dots = 0;
-
-    // Первый проход: подсчитываем количество точек
-    for (int i = 0; i < len; i++) {
-        if (text[i] == '.') {
-            dots++;
-        }
-    }
- spaceCounts = (int*)malloc(dots * sizeof(int));
-    // Второй проход: заполняем массив
-    dots = 0;
-    spaceCounter = 0;
-
-    for (int i = 0; i <= len; i++) {
-        if (text[i] == '.' || text[i] == '\0') {
-            spaceCounts[dots++] = spaceCounter;
-            spaceCounter = 0;
-        }
-        else if (text[i] == ' ') {
-            spaceCounter++;
-        }
-    }
-
-    *dotCount = dots;
-    return spaceCounts;
-}
-int shotchic(int *spaceCounts,int dotCount){
-    int shotchic=0;
-    int i;
-    for (int i = 1; i < dotCount-2; i++) {
-            if (spaceCounts[i-1]-8 < spaceCounts [i] || spaceCounts [i] < spaceCounts[i-1]+8){
-            shotchic++;
-            }
-        }
-    return shotchic;
-}
 int main() {
     char text[] = "Это предложение с тремя пробелами до точки. Второе с двумя пробелами. Третье. Четвертое предложение с четырьмя пробелами до точки.";
     int dotCount;
