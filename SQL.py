2cdeb1c57b3dc3b11c6d844d7885f63f32ddd0e0
Mishka375 <146991783+Mishka375@users.noreply.github.com>
2026-03-18 20:58:43 +0300

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -6,14 +6,22 @@ cursor = conn.cursor()
 
 # Создание талицы
 cursor.execute('''
-CREATE TABLE Words (
+CREATE TABLE Work (
+    id INT PRIMARY KEY AUTOINCREMENT,
     word TEXT NOT NULL,
     count INT
 )
 ''')
 
 # Чтение данных
-cursor.execute("CREATE TABLE Work AS SELECT * FROM Words ORDER BY count DESC")
+cursor.execute("INSERT INTO Work (word, count) SELECT * FROM Words ORDER BY count DESC;")
+cursor.execute("SELECT COUNT(*) FROM Work")
+
+for i in range(cursor.fetchall()):
+    cursor.execute(f"SELECT word FROM Work WHERE id = {i+1};")
+    inf = cursor.fetchall()
+
+    Что-то делается
 
 # Сохранение данных и закрытие соединения
 conn.commit()
