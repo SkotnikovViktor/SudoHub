75afdd1b705af6ba88a97882fabd36c31336c643
WasiliiSizov <V8261261@gmail.com>
2026-03-27 10:19:03 +0300

    доделана разрывнасть, можно вставлять в программу

--- Clib/ToolsForCompile/main.py
+++ Clib/ToolsForCompile/main.py
@@ -1,10 +1,16 @@
 import alg_wrapper
 
+# 1. Поиск подстроки
 text = "abracadabra"
 pattern = "abra"
-print(alg_wrapper.knut_morris_pratta(pattern, text)) 
+print("Количество вхождений:", alg_wrapper.knut_morris_pratta(pattern, text))
 
-s = "  Hello.   World.  "
-print(alg_wrapper.count_spaces_before_each_dot(s))   
+# 2. Подсчёт пробелов перед точками
+s = "Hello world.This is a test.Another sentence."
+spaces = alg_wrapper.count_spaces_before_each_dot(s)
+print("Пробелы перед точками:", spaces)
 
-
+print(len(spaces))
+# 3. Вычисление "shotchic"
+result = alg_wrapper.py_shotchic(spaces)+1
+print("Результат shotchic:", result)
