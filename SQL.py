e85e87a41d1331c21df1077f5820bf5495488c14
Mishka375 <146991783+Mishka375@users.noreply.github.com>
2026-03-27 09:45:36 +0300

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,31 +1,35 @@
-import sqlite3
+import sqlite3, alg_wrapper
 
-# Подключение к DataBase
-conn = sqlite3.connect('Data/Main.db')
-cursor = conn.cursor()
-
-# Удаление прошлой таблицы
-cursor.execute("DROP TABLE IF EXISTS Work")
-
-# Создание талицы
-cursor.execute("CREATE TABLE IF NOT EXISTS Work (word TEXT NOT NULL, count INT);")
-
-# Чтение данных
-cursor.execute("INSERT INTO Work (word) SELECT * FROM Words;")
-cursor.execute("SELECT word FROM Work")
-
-words = cursor.fetchall()
-
-for i in range(len(words)):
+def search (text):
+    # Подключение к DataBase
+    conn = sqlite3.connect('Data/Main.db')
+    cursor = conn.cursor()
     
-    word = words[i][0]
+    # Удаление прошлой таблицы
+    cursor.execute("DROP TABLE IF EXISTS Work")
+    
+    # Создание талицы
+    cursor.execute("CREATE TABLE IF NOT EXISTS Work (word TEXT NOT NULL, count INT);")
+    
+    # Чтение данных
+    cursor.execute("INSERT INTO Work (word) SELECT * FROM Words;")
+    cursor.execute("SELECT word FROM Work")
+    
+    words = cursor.fetchall()
+    
+    for i in range(len(words)):
+        
+        word = words[i][0]
+    
+        count = alg_wrapper.knut_morris_pratta(word, text)
+    
+        cursor.execute(f"UPDATE Work SET count = {count} WHERE word = {word};")
+    
+    result = cursor.execute("SELECT * FROM Work")
 
-    count = vasily_program(word)
-
-    cursor.execute(f"UPDATE Work SET count = {count} WHERE word = {word};")
-
-# Что сделать с количеством надо?
-
-# Сохранение данных и закрытие соединения
-conn.commit()
-conn.close()
+    for i in range(len(result)):
+        print(f"{i[0]}:{i[1]}")
+    
+    # Сохранение данных и закрытие соединения
+    conn.commit()
+    conn.close()
